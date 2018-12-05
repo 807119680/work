781a8502eab14ccc14a699b1e7c5bfe8daaedeb2
807119680 <807119680@qq.com>
2018-12-05 09:56:39 +0800

    today
    to practice

--- work/main.c
+++ work/main.c
@@ -7,9 +7,23 @@
 //
 
 #include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
-}
+    int main(int argc, const char * argv[]) {
+        int n,x,num;
+        int flag=-1;
+        scanf("%d%d",&n,&x);
+        for (int i=1; i<=n; i++) {
+            scanf("%d",&num);
+            if (num!=x) {
+                flag++;
+            }
+            else if(num==x){
+                flag++;
+                break;
+            }
+        }
+        if (flag==n-1) {
+            printf("Not Found");
+        }
+        else printf("%d",flag);
+        return 0;
+    }
