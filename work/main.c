74756988aac9f7c313294a47621802f8792274be
807119680 <807119680@qq.com>
2018-12-05 08:46:51 +0800

    Initial Commit

---
+++ work/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  work
+//
+//  Created by s20171106168 on 2018/12/5.
+//  Copyright © 2018 s20171106168. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
