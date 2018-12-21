3ac56232446612b9098944b2ff6a5833c37e9184
807119680 <807119680@qq.com>
2018-12-21 21:15:09 +0800

    qwe
    asd

--- work/main.c
+++ work/main.c
@@ -6,24 +6,4 @@
 //  Copyright © 2018 s20171106168. All rights reserved.
 //
 
-#include <stdio.h>
-    int main(int argc, const char * argv[]) {
-        int n,x,num;
-        int flag=-1;
-        scanf("%d%d",&n,&x);
-        for (int i=1; i<=n; i++) {
-            scanf("%d",&num);
-            if (num!=x) {
-                flag++;
-            }
-            else if(num==x){
-                flag++;
-                break;
-            }
-        }
-        if (flag==n-1) {
-            printf("Not Found");
-        }
-        else printf("%d",flag);
-        return 0;
-    }
+c
